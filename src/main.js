7890b3fe2784d14321f46f6e54bb3fcbb614e5d2
ShedrachJonah11 <shedrachjonah11@gmail.com>
2023-03-14 23:28:06 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,30 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router";
 
-import CounterComponentVue from "./components/CounterComponent.vue";
-import NotFoundPage from "./components/NotFoundPage.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      redirect: "/counter",
-    },
-    {
-      path: "/counter",
-      component: CounterComponentVue,
-    },
-    {
-      path: "/:catchAll(.*)",
-      component: NotFoundPage,
-    },
-  ],
-});
-// createApp(App).use(store)
-// .mount("#app");
 const app = createApp(App);
 app.use(store);
 app.use(router);
